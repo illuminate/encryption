141f4bf5ed59a57f23ea0e5be087a050301eb595
Taylor Otwell <taylorotwell@gmail.com>
2012-10-13 11:52:04 -0500

    use phpSec library for encryption.

--- tests/EncrypterTest.php
+++ tests/EncrypterTest.php
@@ -15,7 +15,7 @@ class EncrypterTest extends PHPUnit_Framework_TestCase {
 
 	protected function getEncrypter()
 	{
-		return new Encrypter('this_is_a_test_key');
+		return new Encrypter(\phpSec\Crypt\Rand::bytes(32));
 	}
 
 }
